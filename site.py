071269d42fe13419dbcb3afe5743bba9c4fcce2d
aubin-tchoi <aubin.tchoi@gmail.com>
2020-12-07 11:26:01 +0100

    Nouvel onglet 'Plans du réseau'

--- site.py
+++ site.py
@@ -7,7 +7,7 @@ sys.path.insert(0, parentdir)
 from flask import Flask,render_template,url_for,request,redirect,json
 
 from load_data.data_to_graph import *
-from python_graph.shortest_path import * #fonctionne tres bien (blibliotheque scipy.csgraph)
+from python_graph.shortest_path import * # fonctionne tres bien (blibliotheque scipy.csgraph)
 from python_graph.class_vertice_graph import *
 
 import display_on_map.Display as Display
@@ -20,8 +20,8 @@ map = folium.Map(
     zoom_start=10)
 
 
-#Pour que ça fonctionne ne pas oublier de faire clic droit,
-#"définir le répertoire courant en accord avec le fichier ouvert dans l'éditeur"
+# Pour que ça fonctionne ne pas oublier de faire clic droit,
+# "définir le répertoire courant en accord avec le fichier ouvert dans l'éditeur"
 
 app = Flask(__name__)
 
@@ -32,12 +32,7 @@ def find_gare(G,gare):
 
 liste_gares_itineraire = []
 
-@app.route('/')
-@app.route('/accueil/')
-def accueil():
-    return render_template("accueil.html")
-
-
+# Affichage des résultats d'une recherche
 @app.route('/affichagecarte/')
 def affiche():
     return render_template("macarte.html")
@@ -46,18 +41,36 @@ def affiche():
 def afficheactualisee():
     return render_template("macarteactualisee.html")
 
+# Affichage des différents plans de réseaux (RER & métro ou Bus)
+@app.route('/affichagereseauRER')
+def affichagereseaRER():
+    return render_template("reseauRER.html")
+
+@app.route('/affichagereseauBus')
+def affichagereseauBus():
+    return render_template("reseauBus.html")
+
+
+# Onglets de la barre de navigation
+@app.route('/')
+@app.route('/accueil/')
+def accueil():
+    return render_template("accueil.html")
 
 @app.route('/itineraire/')
 def itineraire():
-    return render_template("itineraire.html",feuille_route = liste_gares_itineraire)
+    return render_template("itineraire.html", feuille_route = liste_gares_itineraire)
 
+@app.route('/reseau/')
+def reseau():
+    return render_template("reseau.html")
 
 @app.route('/carte/', methods=['GET', 'POST'])
 def carte():
 
-    #On initialise la carte
+    # On initialise la carte
     map = folium.Map(
-    #lat lon folium inversee avec idfm
+    # lat lon folium inversee avec idfm
         location=(48.852186, 2.339754),
         tiles='Stamen Toner',
         zoom_start=10)
@@ -69,8 +82,8 @@ def carte():
 
     if request.method == 'POST':
 
-        #On initialise la liste des gares parcourues
-        #liste_gares_itineraire = []
+        # On initialise la liste des gares parcourues
+        # liste_gares_itineraire = []
 
         origine = request.form['origine']
         destination = request.form['destination']
